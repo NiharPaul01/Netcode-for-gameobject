cbff9d066e3f3e7ed9b7fff7ab25d2f389565346
Nihar <niharpaul09@gmail.com>
2025-08-27 15:46:28 +0530

    spawning object with GameVisualManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,17 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    public event EventHandler<OnClickedOnGridPositionEventArgs> OnClickedOnGridPosition;
+    public class OnClickedOnGridPositionEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+
+    }
     private void Awake() {
         if (Instance != null) {
             Debug.LogError("More than one GameManager instance!");
@@ -14,5 +22,9 @@ public class GameManager : MonoBehaviour
 
     public void ClickedOnGridPosition(int x, int y) {
         Debug.Log("ClickedOnGridPosition" + x + ", " + y);
+        OnClickedOnGridPosition?.Invoke(this, new OnClickedOnGridPositionEventArgs { 
+            x = x,
+            y = y,
+        });
     }
 }
